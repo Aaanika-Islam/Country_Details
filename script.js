a082cfb43d4d6e02cef6489d781faaa1de696dfc
Anika Islam <2220151@iub.edu.bd>
2025-05-09 22:18:04 -0700

    now commit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function start(){
 
     fetch(url)
     .then(res => res.json())
-    .then(res => process(data));
+    .then(data => process(data));
 }
 
 function process(data){
@@ -13,9 +13,24 @@ function process(data){
     oldContent.textContent = "";
     var country = data[0];
 
+    var currencyObj = country.currencies;
+    var currencyKey = Object.keys(currencyObj)[0];
+    var currency = `${currencyObj[currencyKey].name} (${currencyObj[currencyKey].symbol})`;
+ 
+    var languages = Object.values(country.languages).join(", ");
+
     var newDiv = document.createElement("div");
     newDiv.classList.add("innerDivStyle");
 
-    newDiv.innerHTML = `Country Name: ${}`
+    newDiv.innerHTML = `Country Name: ${country.name.common} <br>
+                        Capital City: ${country.capital} <br>
+                        Flag: <img src="${country.flags.png}"> <br>
+                        Currencies: ${currency} <br>
+                        Country Region: ${country.region} <br>
+                        Country Languages: ${languages} <br>
+                        Country Population: ${country.population} <br>`;
+
+    oldContent.appendChild(newDiv);
+
 }
 
